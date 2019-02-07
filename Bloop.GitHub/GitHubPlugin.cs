69f1731084aeece929682e1d3d0fbbce17e22e95
Devin Stiert <devin.stiert@thelevelup.com>
2019-02-06 20:17:24 -0500

    Implementation

--- Bloop.GitHub/GitHubPlugin.cs
+++ Bloop.GitHub/GitHubPlugin.cs
@@ -1,13 +1,17 @@
 ﻿using Bloop.Plugin;
+using Octokit;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Threading.Tasks;
 using System.Windows.Controls;
 
 namespace Bloop.GitHub
 {
     public class GitHubPlugin : IPlugin, ISettingProvider
     {
+        private IReadOnlyList<Repository> _repos;
+
         public Control CreateSettingPanel()
         {
             return new GitHubPluginSettings();
@@ -17,11 +21,19 @@ namespace Bloop.GitHub
         {
             PluginSettings.LoadSettings(Path.Combine(context.CurrentPluginMetadata.PluginDirectory, "settings.json"));
             // PluginSettings.Instance.SettingsChanged = OnSettingsChanged;
+            Task.Run((Action)RefreshRepos);
         }
 
         public List<Result> Query(Query query)
         {
-            throw new NotImplementedException();
+            return null;
+        }
+
+        private void RefreshRepos()
+        {
+            var client = new GitHubClient(new ProductHeaderValue("Bloop.GitHub"));
+            client.Credentials = new Credentials("");
+            _repos = client.Repository.GetAllForCurrent().Result;
         }
     }
 }
